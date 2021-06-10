91f76c522ca9afe35c397de5a41e3511b3bf949f
Staind <85416824+7vp@users.noreply.github.com>
2021-06-10 08:11:24 -0700

    Create loginregister.py

---
+++ loginregister.py
@@ -0,0 +1,40 @@
+#start
+ 
+users = {}
+status = ""
+ 
+def displayMenu():
+    status = input("[BOT] Are you registered user? y/n?")
+    if status == "y":
+        oldUser()
+    elif status == "n":
+        newUser()
+ 
+def newUser():
+    createLogin = input("[BOT] Create your username!: ")
+ 
+    if createLogin in users:
+        print("[BOT] Login name already exist!")
+    else:
+        createPassw = input("[BOT] Create password: ")
+        users[createLogin] = createPassw
+        print("[BOT] User created!")
+ 
+def oldUser():
+    login = input("[BOT] Enter login name: ")
+    passw = input("[BOT] Enter password: ")
+ 
+    if login in users and users[login] == passw:
+        print("[BOT] Login successful!")
+    else:
+        print("[BOT] User doesn't exist or wrong password!")
+ 
+while status != "q":
+    displayMenu()
+    
+#end
+
+
+#Made by Staind
+#Made by Staind
+https://github.com/7vp
